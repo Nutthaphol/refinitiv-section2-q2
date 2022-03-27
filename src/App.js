f4f1da33237dea5d17f4e88d7572ffc458102a40
Nuthaphol Dechpramualphol <nuthapholdechpramualphol@Nuthaphols-MacBook-Air.local>
2022-03-27 22:52:05 +0700

    send

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [data, setData] = useState(undefined);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await axios
+        .get("https://api.publicapis.org/categories")
+        .then((res) => {
+          return res.data;
+        });
+      setData(res);
+    };
+
+    if (!data) {
+      fetchData();
+    }
+  }, [data]);
+
+  const handleOnChangeFilter = (e) => {
+    const value = e.target.value.toLowerCase();
+    setFilter(value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="text-filter">
+        <input
+          value={filter}
+          onChange={handleOnChangeFilter}
+          placeholder="Search"
+        />
+      </div>
+      <div className="container-list">
+        {data &&
+          data.categories
+            .filter((item) => item.toLowerCase().includes(filter) == 1)
+            .map((item, index) => (
+              <div key={index + item} className="list-item">
+                {item}
+              </div>
+            ))}
+      </div>
     </div>
   );
 }
